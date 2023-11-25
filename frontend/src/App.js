d75d78a51944c8fd792afa2f3a95f22c1c7b5edb
ravindra.kumawat <ravindra.kumawat@servicenow.com>
2023-11-25 17:01:33 +0530

    added frontEnd logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import Header from './components/Header';
+import {Container} from 'react-bootstrap';
+import Footer from './components/Footer';
+import {Outlet} from 'react-router-dom';
 
 function App(){
-  return <h1>Welcome to proshop test</h1>
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+        <Outlet/>
+        </Container>
+      </main> 
+      <Footer />
+    </>
+  
+  );
 }
 
 export default App
